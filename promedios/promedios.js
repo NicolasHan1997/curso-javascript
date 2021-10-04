4d00931a9b599445300434f33a2a1a9db18174f3
Nicolas Hansen <nicolas.han.sen@hotmail.com>
2021-10-04 19:59:39 -0300

    creada funcion de moda y html completo

--- promedios/promedios.js
+++ promedios/promedios.js
@@ -1,4 +1,10 @@
 
+function onClickCalcular(objeto) {
+    const numeros = document.getElementById(objeto.input).value.split(',').map(Number);
+    const resultText = document.getElementById(objeto.output);
+    resultText.innerText = "Resultado: " + objeto.funcion(numeros);
+}
+
 function calcularPromedio(lista) {
     let suma = lista.reduce(
         function (acumulado = 0, elemento) {
@@ -19,4 +25,21 @@ function calcularMedia(lista) {
     let media = lista.length % 2 === 0 ? calcularPromedio([lista[mitad - 1], lista[mitad]]) : lista[mitad];
 
     return media;
+}
+
+function calcularModa(lista) {
+    const listaCount = {};
+
+    lista.map(
+        function (elemento) {
+            if (listaCount[elemento]) listaCount[elemento]++;
+            else listaCount[elemento] = 1;
+        }
+    );
+
+    const arrayCount = Object.entries(listaCount).sort(
+        function (a, b) { return a[1] - b[1]; }
+    );
+
+    return arrayCount[arrayCount.length - 1][0];
 }
